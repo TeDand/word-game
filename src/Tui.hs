20f0f2bf92e21dac10d39ba0ea6bf765d3d77e57
TeDand <tejas-d@hotmail.com>
2023-12-01 21:57:16 -0800

    added code to be able to scroll through directory

--- src/Tui.hs
+++ src/Tui.hs
@@ -5,9 +5,13 @@ import Brick
 -- import Brick.Main
 -- import Brick.Types
 -- import Brick.Widgets.Core
+
+import Cursor.Simple.List.NonEmpty
+import qualified Data.List.NonEmpty as NE
 import Graphics.Vty.Attributes
 import Graphics.Vty.Input.Events
 import System.Directory
+import System.Exit
 
 tui :: IO ()
 tui = do
@@ -16,7 +20,7 @@ tui = do
   print endState
 
 data TuiState = TuiState
-  { tiuStatePaths :: [FilePath]
+  { tuiStatePaths :: NonEmptyCursor FilePath
   }
   deriving (Show, Eq)
 
@@ -29,24 +33,50 @@ tuiApp =
       appChooseCursor = showFirstCursor,
       appHandleEvent = handleTuiEvent,
       appStartEvent = return (),
-      appAttrMap = const $ attrMap defAttr []
+      appAttrMap = const $ attrMap defAttr [(attrName "selected", fg red)]
     }
 
 buildInitialState :: IO TuiState
 buildInitialState = do
   here <- getCurrentDirectory
   contents <- listDirectory here
-  pure TuiState {tiuStatePaths = contents}
+  case NE.nonEmpty contents of
+    Nothing -> die "Empty directory"
+    Just ne ->
+      pure TuiState {tuiStatePaths = makeNonEmptyCursor ne}
 
 drawTui :: TuiState -> [Widget ResourceName]
-drawTui ts = [vBox $ map drawPath $ tiuStatePaths ts]
+drawTui ts =
+  [ vBox $
+      concat
+        [ map (drawPath False) $ reverse $ nonEmptyCursorPrev nec,
+          [drawPath True $ nonEmptyCursorCurrent nec],
+          map (drawPath False) $ nonEmptyCursorNext nec
+        ]
+  ]
+  where
+    nec = tuiStatePaths ts
 
-drawPath :: FilePath -> Widget n
-drawPath = str
+-- [vBox $ map drawPath $ tuiStatePaths ts]
+
+drawPath :: Bool -> FilePath -> Widget n
+drawPath b = (if b then withAttr (attrName "selected") else id) . str
 
 handleTuiEvent :: BrickEvent n e -> EventM n TuiState ()
 handleTuiEvent e = case e of
   VtyEvent vtye -> case vtye of
     EvKey (KChar 'q') [] -> halt
+    EvKey KDown [] -> do
+      ts <- get
+      let nec = tuiStatePaths ts
+      case nonEmptyCursorSelectNext nec of
+        Just nec' -> modify $ \s -> s {tuiStatePaths = nec'}
+        Nothing -> return ()
+    EvKey KUp [] -> do
+      ts <- get
+      let nec = tuiStatePaths ts
+      case nonEmptyCursorSelectPrev nec of
+        Just nec' -> modify $ \s -> s {tuiStatePaths = nec'}
+        Nothing -> return ()
     _ -> return ()
   _ -> return ()

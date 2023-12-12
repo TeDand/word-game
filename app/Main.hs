b14e92ca1dd811e49ff4c0cbc86c0223113f8e41
Baoping Wan <b2wan@ucsd.edu>
2023-12-12 15:49:04 -0800

    merge menu

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main (main) where
 
-import Dataloader (Difficulty (Easy, Hard, Nightmare))
-import Scoreboard
-import Tui (tui)
+import Menu
 
 -- main :: IO Int
 -- main = do
@@ -18,27 +16,4 @@ import Tui (tui)
 --   print (writtenBoard)
 
 main :: IO ()
--- main = tui
-main = do
-  putStrLn "Press Enter to Start the Game"
-  _ <- getLine
-  putStrLn "Select your difficulty level! Easy, Hard, Nightmare" -- Should be in a menu
-  userInput <- getLine
-  difficulty <- convertDifficulty userInput
-
-  finalScore <- tui difficulty -- Wtf how do I pass an argument in
-  scoreBoard <- readScoreboard
-  putStrLn "Enter your username to save your score!"
-  username <- getLine
-
-  writtenBoard <- writeScoreboard (addNewScore scoreBoard username finalScore)
-  putStrLn "Current Scoreboard:"
-  print writtenBoard
-
--- convertDifficulty is a placeholder and the menu should feed the real difficulty level in
-convertDifficulty :: String -> IO Difficulty
-convertDifficulty diff = case diff of
-  "Easy" -> return Easy
-  "Hard" -> return Hard
-  "Nightmare" -> return Nightmare
-  _ -> return Easy
+main = menu 1
---
+++ src/Menu.hs
@@ -0,0 +1,117 @@
+module Menu
+  ( menu,
+  )
+where
+
+import Tui
+import Brick
+import Dataloader
+import Scoreboard
+import qualified Graphics.Vty as V
+
+dummy :: Difficulty -> IO Int
+dummy _ = return 0
+
+tr :: String -> Widget ()
+tr x = str x
+
+fm :: Int -> Difficulty
+fm i = case i of
+        1 -> Easy
+        2 -> Hard
+        3 -> Nightmare
+        _ -> error "impossibile difficulty level"
+
+menu :: Int -> IO ()
+menu i = do
+  x <- topMain $ tr top
+  sb <- readScoreboard
+  case x of
+    "b" -> do 
+            s <- tui $ fm i
+            n <- nameMain $ tr name
+            if n/="" then writeScoreboard (addNewScore sb n s) else return ()
+            menu i
+    "s" -> do
+            simpleMain $ tr $ "press any key to quit\n" ++ (show sb)
+            menu i
+    "o" -> do
+            d <- diffMain i $ tr diff
+            menu d
+    "q" -> return ()
+    _ -> error "?"
+
+top :: String
+top = "Hello, welcome to play Wordgame!\n\
+      \press b to begin play\n\
+      \press s to see the scoreboard\n\
+      \press o to change option\n\
+      \press q to quit."
+
+topMain :: Widget () -> IO String
+topMain w = defaultMain (topApp w) ""
+
+topApp :: Widget () -> App String e ()
+topApp w =
+    App { appDraw = const [w]
+        , appHandleEvent = topHandle
+        , appStartEvent = return ()
+        , appAttrMap = const $ attrMap V.defAttr []
+        , appChooseCursor = neverShowCursor
+        }
+
+topHandle :: BrickEvent () e -> EventM () String ()
+topHandle (VtyEvent (V.EvKey (V.KChar 'b') [])) = do put "b"; halt
+topHandle (VtyEvent (V.EvKey (V.KChar 's') [])) = do put "s"; halt
+topHandle (VtyEvent (V.EvKey (V.KChar 'o') [])) = do put "o"; halt
+topHandle (VtyEvent (V.EvKey (V.KChar 'q') [])) = do put "q"; halt
+topHandle _ = continueWithoutRedraw
+
+name :: String
+name = "Thank you for playing.\n\
+        \Type your name to save your score in the scoreborad.\n\
+        \Click esc when you finish.\n\
+        \If you don't want to save your score, click esc without type your name."
+
+nameMain :: Widget () -> IO String
+nameMain w = defaultMain (nameApp w) ""
+
+nameApp :: Widget () -> App String e ()
+nameApp w =
+    App { appDraw = const [w]
+        , appHandleEvent = nameHandle
+        , appStartEvent = return ()
+        , appAttrMap = const $ attrMap V.defAttr []
+        , appChooseCursor = neverShowCursor
+        }
+
+nameHandle :: BrickEvent () e -> EventM () String ()
+nameHandle (VtyEvent (V.EvKey V.KEsc [])) = halt
+nameHandle (VtyEvent (V.EvKey (V.KChar c) [])) = do s <- get; put $ s++[c]
+nameHandle _ = continueWithoutRedraw
+
+diff :: String
+diff= "Input a number to select the difficulty level\n\
+      \1: Easy\n\
+      \2: Hard\n\
+      \3: Nightmare\n\ 
+      \Press esc to cancel."
+
+diffMain :: Int -> Widget () -> IO Int
+diffMain i w = defaultMain (diffApp w) i
+
+diffApp :: Widget () -> App Int e ()
+diffApp w =
+    App { appDraw = const [w]
+        , appHandleEvent = diffHandle
+        , appStartEvent = return ()
+        , appAttrMap = const $ attrMap V.defAttr []
+        , appChooseCursor = neverShowCursor
+        }
+
+diffHandle :: BrickEvent () e -> EventM () Int ()
+diffHandle (VtyEvent (V.EvKey V.KEsc [])) = halt
+diffHandle (VtyEvent (V.EvKey (V.KChar '1') [])) = do put 1; halt
+diffHandle (VtyEvent (V.EvKey (V.KChar '2') [])) = do put 2; halt
+diffHandle (VtyEvent (V.EvKey (V.KChar '3') [])) = do put 3; halt
+diffHandle _ = continueWithoutRedraw

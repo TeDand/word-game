86c6ee4cc30f04df1ee2d1796ccf18a26d5f1dd3
Baoping Wan <b2wan@ucsd.edu>
2023-12-13 11:41:30 -0800

    format change

--- src/Shuffle.hs
+++ src/Shuffle.hs
@@ -13,13 +13,13 @@ shuffle x = imp x []
 imp :: [String] -> [String] -> IO [String]
 imp [] y = return y
 imp x y = do
-    n <- getStdRandom (randomR (0,(length x) - 1))
+    n <- getStdRandom $ randomR (0,(length x) - 1)
     let (a,b) = splitAt n x
     imp (a ++ tail b) ((x!!n):y)
 
 randomWordGenerator :: Int -> IO String
 randomWordGenerator 0 = return ""
 randomWordGenerator i = do
-  n <- getStdRandom (randomR (0, 25))
+  n <- getStdRandom $ randomR (0, 25)
   l <- randomWordGenerator $ i-1
   return $ (chr $ ord 'a' + n) : l

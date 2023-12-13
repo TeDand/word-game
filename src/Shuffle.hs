747f5c8ea50a77e8f10b521a9cd20a63a9a0e6bd
Baoping Wan <b2wan@ucsd.edu>
2023-12-13 11:29:59 -0800

    add rng

--- src/Shuffle.hs
+++ src/Shuffle.hs
@@ -1,9 +1,11 @@
 module Shuffle
   ( shuffle,
+    randomWordGenerator,
   )
 where
 
 import System.Random
+import Data.Char
 
 shuffle :: [String] -> IO [String]
 shuffle x = imp x []
@@ -11,6 +13,13 @@ shuffle x = imp x []
 imp :: [String] -> [String] -> IO [String]
 imp [] y = return y
 imp x y = do
-    n <- randomRIO (0,(length x) - 1)
+    n <- getStdRandom (randomR (0,(length x) - 1))
     let (a,b) = splitAt n x
     imp (a ++ tail b) ((x!!n):y)
+
+randomWordGenerator :: Int -> IO String
+randomWordGenerator 0 = return ""
+randomWordGenerator i = do
+  n <- getStdRandom (randomR (0, 25))
+  l <- randomWordGenerator $ i-1
+  return $ (chr $ ord 'a' + n) : l
